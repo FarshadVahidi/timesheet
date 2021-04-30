80b17007385098229f2d6cf2b7a79aa8068af995
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-04-30 13:00:47 +0200

    up to github

--- app/Providers/Activiti/ActivityProvider.php
+++ app/Providers/Activiti/ActivityProvider.php
@@ -27,4 +27,57 @@ class ActivityProvider extends ServiceProvider
     {
         //
     }
+
+    public function edit($id)
+    {
+        $alldata = Event::select([
+            \DB::raw("DATE_FORMAT(start, '%Y-%m') as month"),
+            \DB::raw('SUM(hour) as amount'),
+            'user_id',
+        ])
+            ->where('user_id', '=', $id)
+            ->groupBy('month', 'user_id')
+            ->orderBy('month')
+            ->get();
+        $report = [];
+
+        $alldata->each(function ($item) use (&$report) {
+            $report[$item->month][$item->hour] = [
+                'amount' => $item->amount
+            ];
+        });
+        return $alldata;
+    }
+
+    public function month($id, $dt, $de)
+    {
+        $alldata = Event::select([
+            \DB::raw("DATE_FORMAT(start, '%Y-%m') as month"),
+            \DB::raw('SUM(hour) as amount')
+        ])
+            ->where('user_id', '=', $id)
+            ->where('start', '>=', $dt)
+            ->where('start', '<', $de)
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get()->pluck('amount');
+
+        return $alldata;
+    }
+
+    public function monthPDF($id, $dt, $de)
+    {
+        $data = DB::table('events')
+            ->select([
+                \DB::raw("DATE_FORMAT(start, '%Y-%m-%d') as month"),
+                'hour',
+                'title'
+            ])
+            ->where('start', '>=' , $dt)
+            ->where('start', '<', $de)
+            ->where('user_id', '=' , $id)
+            ->orderBy('start')
+            ->get();
+        return $data;
+    }
 }
